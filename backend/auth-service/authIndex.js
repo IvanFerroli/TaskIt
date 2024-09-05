96175a15c8f0d21144d7f7896d7e84d24908787d
Ivanilson Ferreira <ivanilson.ferreira.mec@gmail.com>
2024-09-05 01:54:02 -0300

    fix: documents name minor modifications

--- backend/auth-service/authIndex.js
+++ backend/auth-service/authIndex.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const sequelize = require('./config/database');
+const sequelize = require('./config/authDataBase');
 const authRoutes = require('./routes/authRoutes');
 require('dotenv').config();
 
